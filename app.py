10889a9aca8d6041be1dcad49f1e60781bd2a72b
Divyansh Sharma <divyanshsharma2203@gmail.com>
2024-10-07 19:53:05 +0530

    Refactor: removed redundant code

--- app.py
+++ app.py
@@ -30,50 +30,23 @@ def predict_datapoint():
         # Check if the model for the selected ticker is already trained
         model_file_path = os.path.join(PRETRAINED_MODEL_DIR, f'{model_name}_{ticker}.keras')
 
-        if os.path.exists(model_file_path):
-            # Model exists, proceed to prediction
-            logging.info(f"Model for {ticker} already exists. Proceeding with prediction.")
+        # if os.path.exists(model_file_path):
+        # Model exists, proceed to prediction
+        logging.info(f"Model for {ticker} already exists. Proceeding with prediction.")
 
-            # Initialize the custom data and predict pipeline
-            # data = CustomData(ticker=ticker, model_name=model_name)
-            # pred_df = data.get_data_as_data_frame()
+        # Initialize the custom data and predict pipeline
+        # data = CustomData(ticker=ticker, model_name=model_name)
+        # pred_df = data.get_data_as_data_frame()
 
-            # Initialize PredictPipeline
-            predict_pipeline = PredictPipeline()
+        # Initialize PredictPipeline
+        predict_pipeline = PredictPipeline()
 
-            # Perform prediction
-            logging.info("Running prediction...")
-            results = predict_pipeline.predict_next_day(symbol=ticker, model_type=model_name)
+        # Perform prediction
+        logging.info("Running prediction...")
+        results = predict_pipeline.predict_next_day(symbol=ticker, model_type=model_name)
 
-            logging.info(f"Prediction results: {results}")
-            return render_template('home.html', results=results)
-
-        else:
-            # Model does not exist, train the model first
-            logging.warning(f"Model for {ticker} does not exist. Starting training process...")
-
-            # Initialize training pipeline and notify user of the training
-            train_pipeline = TrainPipeline()
-            try:
-                train_pipeline.run_training_pipeline(ticker, model_name)  # Training the model for the ticker
-
-                # Once training is complete, proceed with prediction
-                logging.info(f"Model for {ticker} is trained. Proceeding with prediction.")
-
-                # Initialize PredictPipeline
-                predict_pipeline = PredictPipeline()
-
-                # Perform prediction
-                logging.info("Running prediction after training...")
-                results = predict_pipeline.predict_next_day(symbol=ticker, model_type=model_name)
-
-                logging.info(f"Prediction results after training: {results}")
-                return render_template('home.html', results=results[0], message="Model training complete. Prediction complete.")
-
-            except Exception as e:
-                logging.error(f"Error during training: {e}", exc_info=True)
-                return render_template('home.html', message=f"Error in prediction for {ticker}. Please try again later.")
-
+        logging.info(f"Prediction results: {results}")
+        return render_template('home.html', results=results)
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", debug=True)  # for deployment
